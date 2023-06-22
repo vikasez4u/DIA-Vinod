d8696145c2e3188fb0a28e18d53a91d1493cfa9b
Dhir, Manish <manish.dhir4@gmail.com>
2023-06-22 17:25:45 +0530

    UI Improvement and misc fixes

--- DIA/src/app/baised/baised.component.ts
+++ DIA/src/app/baised/baised.component.ts
@@ -9,5 +9,19 @@ import {Router} from '@angular/router';
 export class BaisedComponent {
      url= 'Please Enter URL';
 
+     name='';
+     selectedElement={id:-1, Name:''};
+
+     handleClear(){
+      this.name='';
+      this.selectedElement= {id:-1, Name:'Select...'};
+     }
+
+    types:any[]=[
+      {id:-1, Name:'Select...'},
+      {id:1, Name:'Image'},
+      {id:2, Name:'Text'}
+    ];
 
 }
+
