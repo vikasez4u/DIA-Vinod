69e8929369a49176dcfcd61ba31e4ca646b4c8df
SMITAMAITY <smitam996@gmail.com>
2024-07-24 11:26:52 +0530

    Some UI changes for header

--- DIA/src/app/header/header.component.ts
+++ DIA/src/app/header/header.component.ts
@@ -20,18 +20,18 @@ activeBtn(btnActive: string) {
     if(this.btnActive =='one'){
     this.router.navigate( ['home']);
     }
-  if(this.btnActive =='three'){
+  else if(this.btnActive =='three'){
     this.router.navigate( ['about']);
    /* this.sectionScroll='subContent';
     this.router.navigate( ['home' ], {fragment: 'subContent'});*/
   }
-if(this.btnActive == 'two'){
+else if(this.btnActive == 'two'){
   this.router.navigate(['config']);
   }
-if(this.btnActive == 'four'){
+else if(this.btnActive == 'four'){
   this.router.navigate(['faq']);
   }
-if(this.btnActive == 'five'){
+else if(this.btnActive == 'five'){
   this.router.navigate(['contact']);
   }
 }
